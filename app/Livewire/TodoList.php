94a74b415a54ff75de6846bd852d35fca8a5c2eb
Fikri <fikrizhanking@gmail.com>
2023-10-18 16:27:00 +0700

    Create todo function

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class TodoList extends Component
 {
+    #[Rule('required|string|max:120')]
+    public $title;
+
+    public function create()
+    {
+        $validated = $this->validateOnly('title');
+        
+        Todo::create($validated);
+
+        $this->reset('title');
+
+        session()->flash('success', 'Todo was saved!');
+    }
+
     public function render()
     {
         return view('livewire.todo-list');
--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -10,7 +10,7 @@ class Todo extends Model
     use HasFactory;
 
     protected $fillable = [
-        'text',
+        'title',
         'completed'
     ];
 }
